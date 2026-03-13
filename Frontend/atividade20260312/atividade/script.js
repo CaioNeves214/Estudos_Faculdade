f8dc8aa942eecd8c790708d2a4148c54a73e7278
Laboratorio Unidade Fatima <labunivas@univas.edu.br>
2026-03-12 21:53:27 -0300

    Atividade de DOM

---
+++ Frontend/atividade20260312/atividade/script.js
@@ -0,0 +1,25 @@
+// 1
+document.querySelector("#btnMensagem").addEventListener("click", function() {
+  window.alert("Bem-vindo ao painel do treinador!");
+});
+
+// 2
+document.querySelector("#btnAlterarTitulo").addEventListener("click", function() {
+  const titulo = document.querySelector("#titulo");
+  titulo.textContent = "Equipe Pokémon carregada!";
+});
+
+// 3
+document.querySelector("#btnTema").addEventListener("click", function() {
+  const body = document.querySelector("body");
+  if (body.style.backgroundColor === "") {
+    body.style.backgroundColor = "blanchedalmond";
+  } else {
+    body.style.backgroundColor = "";
+  }
+});
+
+// 4
+window.onload = function() {
+    document.title = "Painel Pokémon - DOM";
+};
